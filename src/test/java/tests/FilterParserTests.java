8b282c67820f0c05e13d5cc68a565c2850142406
jinifief <jinified@gmail.com>
2016-02-01 18:12:36 +0800

    Edit quotes test to include only Qualifier that accepts String as input

--- src/test/java/tests/FilterParserTests.java
+++ src/test/java/tests/FilterParserTests.java
@@ -44,11 +44,7 @@ public class FilterParserTests {
     @Test
     public void quotes() {
         // Quoted qualifier content
-        assertEquals(Parser.parse("created:\"a b\""), new Qualifier(CREATED, "a b"));
-        assertEquals(Parser.parse("created:\" > 2014-5-1 \""),
-            new Qualifier(CREATED, new DateRange(LocalDate.of(2014, 5, 1), null, true)));
-        assertEquals(Parser.parse("created:\" 2014-5-1 .. 2014-5-2 \""),
-            new Qualifier(CREATED, new DateRange(LocalDate.of(2014, 5, 1), LocalDate.of(2014, 5, 2))));
+        assertEquals(Parser.parse("body:\"this is\""), new Qualifier(DESCRIPTION, "this is"));
 
         // Prefix quotes
         assertEquals(Parser.parse("\"a b\""), new Qualifier(KEYWORD, "a b"));
